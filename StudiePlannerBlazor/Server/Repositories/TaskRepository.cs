f1a8519f05e4c672e2687692e4f3624ce8b0df4b
Luna Herder <luna@herderfactory.nl>
2021-03-01 12:06:16 +0100

    TaskRepository

--- StudiePlannerBlazor/Server/Repositories/TaskRepository.cs
+++ StudiePlannerBlazor/Server/Repositories/TaskRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StudiePlannerBlazor.Server.DAL;
+using StudiePlannerBlazor.Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,32 @@ namespace StudiePlannerBlazor.Server.Repositories
 {
     public class TaskRepository
     {
+        private readonly ApplicationDBContext _ApplicationDbContext;
+
+        public TaskRepository(ApplicationDBContext appcontext)
+        {
+            _ApplicationDbContext = appcontext;
+        }
+
+        public TaskModel AddTask(TaskModel model)
+        {
+            var addedEntity = _ApplicationDbContext.Add(model);
+            _ApplicationDbContext.SaveChanges();
+            return model;
+        }
+
+        public void DeleteTask(int id)
+        {
+            var foundFotoModel = _ApplicationDbContext.taskModels.FirstOrDefault(p => p.Id == id);
+            if (foundFotoModel == null) return;
+
+            _ApplicationDbContext.taskModels.Remove(foundFotoModel);
+            _ApplicationDbContext.SaveChanges();
+        }
+
+        public IEnumerable<TaskModel> GetAllFotoModellen()
+        {
+            return _ApplicationDbContext.taskModels;
+        }
     }
 }
