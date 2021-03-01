fddb1fd9bb56e66520d4ab22d553991678d5a2b4
hiwad rashad <hiwadrashad112@hotmail.com>
2021-03-01 10:35:00 +0100

    identity update

---
+++ StudiePlannerBlazor/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StudiePlannerBlazor.Server.Data;
+using StudiePlannerBlazor.Server.Models;
+
+[assembly: HostingStartup(typeof(StudiePlannerBlazor.Server.Areas.Identity.IdentityHostingStartup))]
+namespace StudiePlannerBlazor.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
