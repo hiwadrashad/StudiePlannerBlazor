21c8ef5462cb9266044dc7bcd38b00d3a055787e
Flonja <vanjavanessen@icloud.com>
2021-03-02 10:18:57 +0100

    Identity works & fixing models

--- StudiePlannerBlazor/Server/Data/ApplicationDbContext.cs
+++ StudiePlannerBlazor/Server/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using StudiePlannerBlazor.Server.Models;
@@ -22,5 +24,12 @@ namespace StudiePlannerBlazor.Server.Data
         public DbSet<AppointmentModel> Appointments { get; set; }
         public DbSet<CalenderModel> Calenders { get; set; }
         public DbSet<TaskModel> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserLogin>().HasKey(m => m.UserId);
+            builder.Entity<IdentityUserRole>().HasKey(m => m.UserId);
+            base.OnModelCreating(builder);
+        }
     }
 }
---
+++ StudiePlannerBlazor/Shared/Models/DocumentModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudiePlannerBlazor.Shared.Models
+{
+    public class DocumentModel
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+    }
+}
