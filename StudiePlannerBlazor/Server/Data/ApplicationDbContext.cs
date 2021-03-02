24ad093671bd768169bd16557ef4634e5d2dbbf8
Luna Herder <luna@herderfactory.nl>
2021-03-02 10:39:26 +0100

    added two tasks in seed data

--- StudiePlannerBlazor/Server/Data/ApplicationDbContext.cs
+++ StudiePlannerBlazor/Server/Data/ApplicationDbContext.cs
@@ -22,5 +22,27 @@ namespace StudiePlannerBlazor.Server.Data
         public DbSet<AppointmentModel> Appointments { get; set; }
         public DbSet<CalenderModel> Calenders { get; set; }
         public DbSet<TaskModel> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<TaskModel>().HasData(new TaskModel 
+            { 
+                Id = 1, 
+                Name = "Work order for March",
+                StartDate = DateTime.ParseExact("01/03/2021", "dd/MM/yyyy", null),
+                EndDate = DateTime.ParseExact("01/04/2021", "dd/MM/yyyy", null), 
+                Status = Shared.Models.TaskStatus.Busy
+            });
+            modelBuilder.Entity<TaskModel>().HasData(new TaskModel
+            {
+                Id = 2,
+                Name = "Work order for February",
+                StartDate = DateTime.ParseExact("01/02/2021", "dd/MM/yyyy", null),
+                EndDate = DateTime.ParseExact("01/03/2021", "dd/MM/yyyy", null),
+                Status = Shared.Models.TaskStatus.Done
+            });
+        }
     }
 }
