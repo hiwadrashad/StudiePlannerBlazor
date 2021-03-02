0ca977d63ef33a036366c868b6ca34788061bdca
hiwad rashad <hiwadrashad112@hotmail.com>
2021-03-02 12:51:25 +0100

    notifications added

---
+++ StudiePlannerBlazor/Client/Components/NotificationComponentEnd.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudiePlannerBlazor.Client.Components
+{
+    public partial class NotificationComponentEnd
+    {
+        //public string TextToShow { get; set; }
+        //public bool ShowDialog { get; set; }
+    }
+}
--- StudiePlannerBlazor/Client/Pages/CalenderOverview.cs
+++ StudiePlannerBlazor/Client/Pages/CalenderOverview.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Components;
+using StudiePlannerBlazor.Client.Components;
 using StudiePlannerBlazor.Client.DataService;
 using StudiePlannerBlazor.Shared.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace StudiePlannerBlazor.Client.Pages
 {
@@ -13,10 +15,60 @@ namespace StudiePlannerBlazor.Client.Pages
         [Inject]
         public IDataService<TaskModel> TaskDataService { get; set; }
         public List<TaskModel> Tasks { get; set; }
+        private Timer time;
+        protected NotificationComponentStart taskstartednotification { get; set; } = new NotificationComponentStart { ShowDialog = false};
+        protected NotificationComponentEnd tasksendednotification { get; set; } = new NotificationComponentEnd { ShowDialog = false };
 
         protected override async Task OnInitializedAsync()
         {
+            time = new Timer();
+            time.Elapsed += new System.Timers.ElapsedEventHandler(timerexecutioncode);
+            time.Interval = 60000;
+            time.Start();
+
             Tasks = (await TaskDataService.GetAll()).ToList();
+        }
+
+        protected void ShowMessageStart()
+        {
+            taskstartednotification.ShowDialog = true;
+            StateHasChanged();
+        }
+
+        protected void CancelMessageStart()
+        {
+            taskstartednotification.ShowDialog = false;
+            StateHasChanged();
+        }
+
+        protected void ShowMessageEnd()
+        {
+            tasksendednotification.ShowDialog = true;
+            StateHasChanged();
+        }
+
+        protected void CancelMessageEnd()
+        {
+            tasksendednotification.ShowDialog = false;
+            StateHasChanged();
+        }
+        private async void timerexecutioncode(object sender, EventArgs e)
+        {
+            var items = await TaskDataService.GetAll();
+            if (items.Where(a => a.StartDate == DateTime.Now).Any())
+            {
+                var itemtoshow = items.Where(a => a.StartDate == DateTime.Now).FirstOrDefault();
+                string texttoshow = itemtoshow.Name = " " + "has started";
+                taskstartednotification.TextToShow = texttoshow;
+                ShowMessageStart();
+            }
+            if (taskstartednotification.ShowDialog == false)
+            {
+                var itemtoshow = items.Where(a => a.EndDate == DateTime.Now).FirstOrDefault();
+                string texttoshow = itemtoshow.Name = " " + "has ended";
+                tasksendednotification.TextToShow = texttoshow;
+                ShowMessageEnd();
+            }
         }
     }
 }
